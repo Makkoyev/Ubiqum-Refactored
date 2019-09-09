e882ec8fa801acd1daebddbc598f1df031861b7f
Makkoyev <y.makkoyev@gmail.com>
2019-09-09 22:37:17 +0200

    Senate-data.html & House-data.html

---
+++ assets/js/test.js
@@ -0,0 +1,64 @@
+// Check for window name for fetching data
+if (window.location.href.includes("senate") == true) {
+    var url = "https://api.propublica.org/congress/v1/113/senate/members.json"
+} else {
+    var url = "https://api.propublica.org/congress/v1/113/house/members.json"
+}
+
+async function loadData() {
+    const response = await fetch(url, {
+        headers: {
+            "x-api-key": "2VpGVQrEodHgSlKxaOdIhIwh9UWtKcGh9K2fRfAL"
+        }
+    });
+    return await response.json();
+}
+
+loadData().then(data => {
+    let selector = document.getElementById('selector');
+    let members = data.results[0].members
+    // Filter states
+    let mapStates = members.map(member => member.state).sort();
+    let states = [... new Set(mapStates)].forEach(element => {
+        selector.innerHTML += `<option> ${element} </option>`;
+    });
+    // Table generation
+    let table = document.getElementById('table');
+    console.log(table.innerHTML);
+    members.forEach(element => {
+        fullName = () => {
+            if (element.middle_name == null) {
+                return fullName = `${element.first_name} ${element.last_name}`;
+            } else {
+                return fullName = `${element.first_name} ${element.middle_name} ${element.last_name}`;
+            }
+        }
+        table.innerHTML += `<tr><td> ${fullName()} </td><td> ${element.party} </td><td> ${element.state} </td><td> ${element.seniority} </td><td> ${element.votes_with_party_pct} </td></tr>`;
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Filter states 2
+/*
+
+let statesData = [];
+members.forEach(element => {
+    statesData.push(element.state);
+});
+let states = [...new Set(statesData)].sort()
+states.forEach(element => {
+    selector.innerHTML += `<option> ${element} </option>`;
+});
+
+*/
